1a31f6181969ff2ede7d4817023ea374fd16d543
ReformaMark <mark.reforma0117@gmail.com>
2023-03-18 11:40:24 +0800

    add google auth

--- src/components/SocialSignButtons/SocialSignButtons.js
+++ src/components/SocialSignButtons/SocialSignButtons.js
@@ -1,15 +1,75 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import CustomButton from '../CustomButton/CustomButton'
+import { 
+  GoogleAuthProvider, 
+  FacebookAuthProvider, 
+  getAuth, 
+  signInWithCredential, 
+  signInWithPopup } from 'firebase/auth';
+import * as GoogleAuthSession from 'expo-auth-session'
+import { googleLogInConfig } from '../../data/config/googleConfig'
+import { useIdTokenAuthRequest} from 'expo-auth-session/providers/google';
+
 
 
 const SocialSignButtons = () => {
-    
-    const onSigninToGoogle =  () =>{
-        console.warn("You pressed the google")
+  const auth = getAuth();
+  const [request, response, promptAsync] = useIdTokenAuthRequest(
+    googleLogInConfig,
+    {
+      clientId: googleLogInConfig.clientId,
+      scopes: ['openid', 'profile', 'email'],
+      redirectUri: `${GoogleAuthSession.makeRedirectUri()}`,
+    }
+  );
+
+  useEffect(() => {
+    if (response && response.type === 'success') {
+      if (response.provider === 'google') {
+        const { id_token } = response.params;
+        const credential = GoogleAuthProvider.credential(id_token);
+        signInWithCredential(auth, credential)
+          .then((userCredential) => {
+            const user = userCredential.user;
+            // do something with the authenticated user
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      } else if (response.provider === 'facebook') {
+        const credential = FacebookAuthProvider.credentialFromResult(response);
+        signInWithCredential(auth, credential)
+          .then((userCredential) => {
+            const user = userCredential.user;
+            // do something with the authenticated user
+          })
+          .catch((error) => {
+            console.log(error);
+          });
       }
-      const onSigninToFacebook =  () =>{
-        console.warn("You pressed the facebook")
-      }
+    }
+  }, [auth, response]);
+
+  const signinWithGoogle = async () => {
+    try {
+      await promptAsync();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const signInWithFacebook = async () => {
+    try {
+      const provider = new FacebookAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const credential = FacebookAuthProvider.credentialFromResult(result);
+      const user = result.user;
+      // do something with the authenticated user
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
        <CustomButton 
@@ -17,7 +77,7 @@ const SocialSignButtons = () => {
        color='red'
        name='google'
         text="Sign In with Google"
-        onPress={onSigninToGoogle}
+        onPress={signinWithGoogle}
         bgColor="#FAE9EA"
         fgColor="#DD4D44"
         type='SOCIAL'
@@ -27,7 +87,7 @@ const SocialSignButtons = () => {
         color='blue'
         name='facebook-with-circle'
         text="Sign In with Facebook"
-        onPress={onSigninToFacebook}
+        onPress={signInWithFacebook}
         bgColor="#E7EAF4"
         fgColor="#4765A9"
         type='SOCIAL'
